c302e6b6e1674a3e660646a950eb2458fe378ba6
juhriber <j.de.s.r.25@gmail.com>
2021-03-14 16:45:51 -0300

    Finalização do Projeto MilhasParaKm

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Pasta_Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhas;
+            double km = 1.609;
+
+            Console.WriteLine("Digite Seu Valor em Milhas: ");
+            milhas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu Valor de Milhas para KM");
+            Console.WriteLine($"{milhas * km}");
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
